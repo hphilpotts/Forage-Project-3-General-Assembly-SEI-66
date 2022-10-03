2ea3f709d2cc2ac3821cca17ce0591984eba76e7
hvb <helene.vanbesouw@gmail.com>
2022-10-03 09:44:25 +0100

    created spaces in views , models and urls for our coding

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+# image model
+
+
+
+
+
+
+
+
+
+# board model 
+
+
+
+
+
+
+
+
+
+# user profile model 
+
+
+
+
+
+# authentication model 
