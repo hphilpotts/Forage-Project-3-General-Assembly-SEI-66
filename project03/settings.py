f6acac40d71d6d931caa2cc2ea27da40391b0fef
hvb <helene.vanbesouw@gmail.com>
2022-10-06 17:30:50 +0100

    deployment

--- project03/settings.py
+++ project03/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-1u1_i9laxs1gx-h6mk2(d^if^s9le&_5e-j9ic4emdbihk=)#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
