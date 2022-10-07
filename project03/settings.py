0719c29f266ccff4760d039dd5e496c0b96b7806
Ivan Craig <ic188002@falmouth.ac.uk>
2022-10-07 09:20:26 +0100

    fin

--- project03/settings.py
+++ project03/settings.py
@@ -140,5 +140,5 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# import django_heroku
-# django_heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
